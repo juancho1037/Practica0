32df74db46358e59fae233b8d67e6aef67ced9d7
Juan David Rendon <juan.rendon13@udea.edu.co>
2020-09-22 01:28:08 -0500

    Se arreglo el ejercicio 3 que tenia un error aritmetico

--- Ejercicio_3/ejercicio3/main.cpp
+++ Ejercicio_3/ejercicio3/main.cpp
@@ -4,33 +4,50 @@
 
 using namespace std;
 
-void Resultado(int n);
+int potencia(int base, int exp);
 
 int main()
 
 {
     int n;
-
     cout << " Escribe el numero deseado: ";
     cin >> n;
 
-    Resultado(n);
-    return 0;
-}
-
-void Resultado(int n){
-
-    int residuo, suma = 0;
+    int residuo, suma = 0, pot=0;
     float s=1;
 
     while (s!=0)
         {
         s = n/10;
         residuo = n%10;
+        int valor=residuo;
+
+        pot=potencia(residuo,valor);
+
         n = s;
-        suma = suma + (residuo*residuo);
+        suma = suma + pot;
         }
     cout<<endl;
     cout << " El resultado es: " << suma << endl;
     cout<<endl;
+    return 0;
+}
+
+int potencia(int base, int exp){
+    int x=1, resultado=0;
+    if (exp == 1) {
+        resultado +=1;
+    }
+    else {
+        while (x<exp){
+            if (resultado<base){
+                resultado=base*base;
+            }
+            else{
+                resultado=resultado*base;
+            }
+            x++;
+        }
+    }
+    return resultado;
 }
